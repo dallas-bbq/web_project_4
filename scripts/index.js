823f6d7ea0092edc55e612cc0dc97b372a81b37b
Dasha V <dariavolyn@yandex.ru>
2021-02-15 09:00:08 -0500

    Fixes 1st code review

--- scripts/index.js
+++ scripts/index.js
@@ -3,46 +3,38 @@ let editProfileWindow = document.querySelector('.popup');
 let showButton = document.querySelector('.profile__edit');
 let closeButton = document.querySelector('.popup__close');
 let saveButton = document.querySelector('.popup__container-save');
-
-// Show  and close edit profile window
-showButton.addEventListener('click', function showPopup() {
-    editProfileWindow.style.display = 'flex'
-});
-
-function closePopup() {
-    editProfileWindow.style.display = 'none'
-};
-
-closeButton.addEventListener('click', closePopup);
-
-saveButton.addEventListener('click', closePopup);
-
-// Selecting form 
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_about-me');
+let profileName = document.querySelector('.profile__name');
+let profileTitle = document.querySelector('.profile__title');
 let form = document.querySelector('.popup__container');
 
 function handleFormSubmit(evt) {
-    //Stopping default submit
     evt.preventDefault();
 
-    // Selecting name and title input fields
-    let nameInput = document.querySelector('.popup__container-name');
-    let jobInput = document.querySelector('.popup__container-about-me');
+    profileName.textContent = nameInput.value;
+    profileTitle.textContent = jobInput.value;
 
-    //Getting values
-    let nameInputValue = nameInput.value;
-    let jobInputValue = jobInput.value;
-
-    //Getting elements where field values will be enetered
-    let profileName = document.querySelector('.profile__name');
-    let profileTitle = document.querySelector('.profile__title');
-
-    //Inserting new values
-    profileName.textContent = nameInputValue;
-    profileTitle.textContent = jobInputValue;
+    closePopup();
 }
 
-// Connecting handler to the form
 form.addEventListener('submit', handleFormSubmit);
+
+function showPopup() {
+    editProfileWindow.classList.remove('popup_closed')
+    editProfileWindow.classList.add('popup_open')
+    nameInput.value = profileName.textContent
+    jobInput.value = profileTitle.textContent
+};
+
+function closePopup() {
+    editProfileWindow.classList.add('popup_closed')
+    editProfileWindow.classList.remove('popup_open')
+};
+
+showButton.addEventListener('click', showPopup);
+
+closeButton.addEventListener('click', closePopup);
 
 //Selecting like buttons
 let likeButton = document.querySelectorAll('.photos__like');
